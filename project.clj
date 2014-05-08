6fbaf74a3bee7bccc83502f8bf2f79c349370f7b
Peter Taoussanis <ptaoussanis@gmail.com>
2014-05-08 19:48:24 +0700

    v0.13.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso/sente "0.13.0-SNAPSHOT"
+(defproject com.taoensso/sente "0.13.0"
   :author "Peter Taoussanis <https://www.taoensso.com>"
   :description "Clojure channel sockets library"
   :url "https://github.com/ptaoussanis/sente"
