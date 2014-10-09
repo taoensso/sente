99c233325bc0e8f90bb6eb7344c48c72a4b953e2
Luchko Valiantsin <akmeful@gmail.com>
2014-10-09 22:48:37 +0300

    Add example without cljx, just split example code into client and server projects

---
+++ example-project-no-cljx/client/project.clj
@@ -0,0 +1,24 @@
+(defproject sente-test-client "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2342"]
+                 [org.clojure/core.async    "0.1.346.0-17112a-alpha"]
+
+                 [com.cognitect/transit-cljs "0.8.188"]
+
+                 [com.taoensso/sente        "1.2.0"] ; <--- Sente
+                 [com.taoensso/timbre       "3.3.1"]]
+
+  :plugins [[lein-ancient "0.5.5"]
+            [lein-cljsbuild "1.0.4-SNAPSHOT"]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src"]
+        :compiler {
+          :output-to "../server/resources/public/js/main.js"
+          :optimizations :whitespace
+          :pretty-print true}}]})
