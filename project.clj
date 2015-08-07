d3c9eb58c240b9ebf2f9b4cc5a385c7f64c0b9b5
Peter Taoussanis <ptaoussanis@gmail.com>
2015-08-07 23:45:57 +0700

    Bump deps

--- project.clj
+++ project.clj
@@ -14,8 +14,8 @@
   [[org.clojure/clojure      "1.5.1"]
    [org.clojure/core.async   "0.1.346.0-17112a-alpha"]
    [org.clojure/tools.reader "0.9.2"]
-   [com.taoensso/encore      "2.1.2"]
-   [com.taoensso/timbre      "4.0.2"]]
+   [com.taoensso/encore      "2.4.2"]
+   [com.taoensso/timbre      "4.1.0"]]
 
   :profiles
   {;; :default [:base :system :user :provided :dev]
