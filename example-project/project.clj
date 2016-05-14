538177429ac0c301c0fba0771869fea592c679f4
Peter Taoussanis <ptaoussanis@gmail.com>
2016-05-14 19:13:58 +0700

    v1.9.0-SNAPSHOT

--- example-project/project.clj
+++ example-project/project.clj
@@ -1,4 +1,4 @@
-(defproject com.taoensso.examples/sente "1.8.2-alpha1"
+(defproject com.taoensso.examples/sente "1.9.0-SNAPSHOT"
   :description "Sente, reference web-app example project"
   :url "https://github.com/ptaoussanis/sente"
   :license {:name "Eclipse Public License"
@@ -17,7 +17,7 @@
    [org.clojure/core.async    "0.2.374"]
    [org.clojure/tools.nrepl   "0.2.12"] ; Optional, for Cider
 
-   [com.taoensso/sente        "1.8.2-alpha1"] ; <--- Sente
+   [com.taoensso/sente        "1.9.0-SNAPSHOT"] ; <--- Sente
    [com.taoensso/timbre       "4.3.1"]
 
    ;;; ---> Choose (uncomment) a supported web server <---
