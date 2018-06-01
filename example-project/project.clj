7d0b9247f8ff46f4d04ad9d37f0241f854fc11c2
Timothy Pratley <timothypratley@gmail.com>
2018-06-01 10:58:01 -0700

    Static file anti-forgery is not in session, but is in request
    See upstream ring issue resolved:
    https://github.com/ring-clojure/ring-defaults/issues/23
    This change looks for the key in the request which was added in
    ring-anti-forgery 1.3.2, which is in ring defaults 0.3.2
    If you are affected by this issue, make sure you are using Sente 0.13.0+
    and ring defaults 0.3.2+

--- example-project/project.clj
+++ example-project/project.clj
@@ -11,39 +11,39 @@
 
   :dependencies
   [[org.clojure/clojure       "1.9.0"]
-   [org.clojure/clojurescript "1.9.946"]
-   [org.clojure/core.async    "0.3.465"]
+   [org.clojure/clojurescript "1.10.238"]
+   [org.clojure/core.async    "0.4.474"]
    [org.clojure/tools.nrepl   "0.2.13"] ; Optional, for Cider
 
    [com.taoensso/sente        "1.12.0"] ; <--- Sente
    [com.taoensso/timbre       "4.10.0"]
 
    ;;; TODO Choose (uncomment) a supported web server -----------------------
-   [http-kit                             "2.2.0"] ; Default
+   [http-kit                             "2.3.0"] ; Default
    ;; [org.immutant/web                  "2.1.4"]
    ;; [nginx-clojure/nginx-clojure-embed "0.4.4"] ; Needs v0.4.2+
    ;; [aleph                             "0.4.1"]
    ;; -----------------------------------------------------------------------
 
    [ring                      "1.6.3"]
-   [ring/ring-defaults        "0.3.1"] ; Includes `ring-anti-forgery`, etc.
+   [ring/ring-defaults        "0.3.2"] ; Includes `ring-anti-forgery`, etc.
    ;; [ring-anti-forgery      "1.0.0"]
 
-   [compojure                 "1.6.0"] ; Or routing lib of your choice
+   [compojure                 "1.6.1"] ; Or routing lib of your choice
    [hiccup                    "1.0.5"] ; Optional, just for HTML
 
    ;;; Transit deps optional; may be used to aid perf. of larger data payloads
    ;;; (see reference example for details):
-   [com.cognitect/transit-clj  "0.8.300"]
-   [com.cognitect/transit-cljs "0.8.243"]]
+   [com.cognitect/transit-clj  "0.8.309"]
+   [com.cognitect/transit-cljs "0.8.256"]]
 
   :plugins
   [[lein-pprint         "1.2.0"]
-   [lein-ancient        "0.6.14"]
+   [lein-ancient        "0.6.15"]
    [com.cemerick/austin "0.1.6"]
    [lein-cljsbuild      "1.1.7"]
-   [cider/cider-nrepl   "0.15.1"] ; Optional, for use with Emacs
-   ]
+   [cider/cider-nrepl   "0.17.0"]] ; Optional, for use with Emacs
+   
 
   :cljsbuild
   {:builds
