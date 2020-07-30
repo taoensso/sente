216b1669fff7be94c12cb0f0ce89a296b214c153
Peter Taoussanis <ptaoussanis@gmail.com>
2020-07-30 12:59:25 +0200

    Switch to http-kit v2.4.0 server API
    New API avoids a potential race condition, Ref.
    https://github.com/http-kit/http-kit/issues/318

--- src/taoensso/sente/server_adapters/http_kit.clj
+++ src/taoensso/sente/server_adapters/http_kit.clj
@@ -20,17 +20,15 @@
     (let [{:keys [on-open on-close on-msg _on-error]} callbacks-map
           ws? (:websocket? ring-req)]
 
+      ;; Note `as-channel` requires http-kit >= v2.4.0
       ;; Returns {:body <http-kit-implementation-channel> ...}:
-      (hk/with-channel ring-req sch
-        (when on-close (hk/on-close   sch (fn [status-kw] (on-close sch ws? status-kw))))
-        (when on-msg   (hk/on-receive sch (fn [msg]       (on-msg   sch ws? msg))))
-        ;; http-kit channels are immediately open so don't have/need an
-        ;; on-open callback. Do need to place this last though to avoid
-        ;; racey side effects:
-        (when on-open (on-open sch ws?))))))
+      (hk/as-channel ring-req
+        {:on-close   (when on-close (fn [sch status-kw] (on-close sch ws? status-kw)))
+         :on-receive (when on-msg   (fn [sch       msg] (on-msg   sch ws? msg)))
+         :on-open    (when on-open  (fn [sch          ] (on-open  sch ws?)))}))))
 
 (defn get-sch-adapter [] (HttpKitServerChanAdapter.))
 
 (enc/deprecated
-  (def http-kit-adapter "Deprecated" (get-sch-adapter))
+  (def http-kit-adapter         "Deprecated" (get-sch-adapter))
   (def sente-web-server-adapter "Deprecated" http-kit-adapter))
