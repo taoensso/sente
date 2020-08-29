faa6b58181ad339419bcece83fcd9dc7bd74dfde
Peter Taoussanis <ptaoussanis@gmail.com>
2020-08-29 19:22:51 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -11,8 +11,7 @@
                 *assert* true}
 
   :dependencies
-  [[org.clojure/clojure      "1.10.1" :scope "provided"]
-   [org.clojure/core.async   "1.3.610"]
+  [[org.clojure/core.async   "1.3.610"]
    [com.taoensso/encore      "2.122.0"]
    [org.java-websocket/Java-WebSocket "1.5.1"]
    [org.clojure/tools.reader "1.3.3"]
@@ -27,25 +26,24 @@
   :profiles
   {;; :default [:base :system :user :provided :dev]
    :server-jvm {:jvm-opts ^:replace ["-server"]}
-   :1.8  {:dependencies [[org.clojure/clojure "1.8.0"]]}
-   :1.9  {:dependencies [[org.clojure/clojure "1.9.0"]]}
-   :1.10 {:dependencies [[org.clojure/clojure "1.10.1"]]}
-   :test {:dependencies [[com.cognitect/transit-clj  "1.0.324"]
-                         [com.cognitect/transit-cljs "0.8.264"]
-                         [org.clojure/test.check     "1.1.0"]]}
-
-   :provided {:dependencies [[org.clojure/clojurescript "1.10.773"]]}
-
-   :dev
-   [:1.10 :test :server-jvm
-    {:dependencies
-     [[http-kit         "2.4.0"]
-      [org.immutant/web "2.1.10"]
-      [nginx-clojure    "0.5.1"]
-      [aleph            "0.4.6"]
-      [macchiato/core   "0.2.20"]
-      [luminus/ring-undertow-adapter  "1.1.2"]
-      [info.sunng/ring-jetty9-adapter "0.14.0"]]}]}
+   :provided {:dependencies [[org.clojure/clojure       "1.10.1"]
+                             [org.clojure/clojurescript "1.10.773"]]}
+   :1.8      {:dependencies [[org.clojure/clojure "1.8.0"]]}
+   :1.9      {:dependencies [[org.clojure/clojure "1.9.0"]]}
+   :1.10     {:dependencies [[org.clojure/clojure "1.10.1"]]}
+   :depr     {:jvm-opts ["-Dtaoensso.elide-deprecated=true"]}
+   :dev      [:1.10 :test :server-jvm :depr]
+   :test     {:dependencies
+              [[com.cognitect/transit-clj  "1.0.324"]
+               [com.cognitect/transit-cljs "0.8.264"]
+               [org.clojure/test.check     "1.1.0"]
+               [http-kit         "2.4.0"]
+               [org.immutant/web "2.1.10"]
+               [nginx-clojure    "0.5.1"]
+               [aleph            "0.4.6"]
+               [macchiato/core   "0.2.20"]
+               [luminus/ring-undertow-adapter  "1.1.2"]
+               [info.sunng/ring-jetty9-adapter "0.14.0"]]}}
 
   :cljsbuild
   {:test-commands {"node"    ["node" :node-runner "target/main.js"]
@@ -60,9 +58,13 @@
   :test-paths ["test" "src"]
 
   :aliases
-  {"build-once" ["cljsbuild" "once"]
+  {"start-dev"  ["with-profile" "+dev" "repl" ":headless"]
+   "build-once" ["cljsbuild" "once"]
    "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]
-   "start-dev"  ["with-profile" "+dev" "repl" ":headless"]}
+   "test-all"   ["do" "clean,"
+                 "with-profile" "+1.10:+1.9:+1.8"   "test,"
+                 "with-profile" "+test" "cljsbuild" "test"]}
 
   :repositories
-  {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"})
+  {"sonatype-oss-public"
+   "https://oss.sonatype.org/content/groups/public/"})
