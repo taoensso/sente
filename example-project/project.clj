f99ec21597a1ed4c3aa5755bc7307f3eb440c302
Peter Taoussanis <peter@taoensso.com>
2022-05-31 09:59:06 +0200

    [Example project] Bump deps

--- example-project/project.clj
+++ example-project/project.clj
@@ -10,16 +10,16 @@
                 *assert* true}
 
   :dependencies
-  [[org.clojure/clojure       "1.10.2"]
-   [org.clojure/clojurescript "1.10.773"]
-   [org.clojure/core.async    "1.3.610"]
+  [[org.clojure/clojure       "1.11.1"]
+   [org.clojure/clojurescript "1.11.54"]
+   [org.clojure/core.async    "1.5.648"]
    [org.clojure/tools.nrepl   "0.2.13"] ; Optional, for Cider
 
    [com.taoensso/sente        "1.16.2"] ; <--- Sente
-   [com.taoensso/timbre       "5.1.2"]
+   [com.taoensso/timbre       "5.2.1"]
 
    ;;; TODO Choose (uncomment) a supported web server -----------------------
-   [http-kit                             "2.5.3"] ; Default
+   [http-kit                             "2.6.0-RC1"] ; Default
    ;; [org.immutant/web                  "2.1.10"
    ;;  :exclusions [ring/ring-core]]
    ;; [nginx-clojure/nginx-clojure-embed "0.5.2"] ; Needs v0.4.2+
@@ -27,24 +27,24 @@
    ;; [info.sunng/ring-jetty9-adapter    "0.14.2"]
    ;; -----------------------------------------------------------------------
 
-   [ring                      "1.9.1"]
-   [ring/ring-defaults        "0.3.2"] ; Includes `ring-anti-forgery`, etc.
+   [ring                      "1.9.5"]
+   [ring/ring-defaults        "0.3.3"] ; Includes `ring-anti-forgery`, etc.
    ;; [ring-anti-forgery      "1.3.0"]
 
-   [compojure                 "1.6.2"] ; Or routing lib of your choice
+   [compojure                 "1.7.0"] ; Or routing lib of your choice
    [hiccup                    "1.0.5"] ; Optional, just for HTML
 
    ;;; Transit deps optional; may be used to aid perf. of larger data payloads
    ;;; (see reference example for details):
-   [com.cognitect/transit-clj  "1.0.324"]
-   [com.cognitect/transit-cljs "0.8.264"]]
+   [com.cognitect/transit-clj  "1.0.329"]
+   [com.cognitect/transit-cljs "0.8.269"]]
 
   :plugins
   [[lein-pprint         "1.3.2"]
    [lein-ancient        "0.7.0"]
    ;[com.cemerick/austin "0.1.6"]
    [lein-cljsbuild      "1.1.8"]
-   [cider/cider-nrepl   "0.25.9"]] ; Optional, for use with Emacs
+   [cider/cider-nrepl   "0.28.4"]] ; Optional, for use with Emacs
 
   :cljsbuild
   {:builds
